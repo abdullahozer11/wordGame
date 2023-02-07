a28282c51021cd87548c8eb87281b1603063f07b
Abdullah OZER <abdullahozer11@hotmail.com>
2023-02-07 02:25:26 +0100

    score js is inited

---
+++ src/components/Score/Score.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Score extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+Score.propTypes = {};
+
+export default Score;
