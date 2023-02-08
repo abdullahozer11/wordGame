b689ba75d1dfd5bdf541ec75e07a6a094849e29d
Abdullah OZER <abdullahozer11@hotmail.com>
2023-02-08 22:44:58 +0100

    html code moved to component

--- src/components/LetterCube/LetterCube.js
+++ src/components/LetterCube/LetterCube.js
@@ -1,50 +1,58 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import "./LetterCube.css";
 
+const box = document.querySelector(".box");
 
 class LetterCube extends Component {
     constructor(props) {
         super(props);
-    }
-
-    componentWillMount() {
-
+        this.state = {
+            index: 0,
+        }
     }
 
     componentDidMount() {
-
-    }
-
-    componentWillReceiveProps(nextProps) {
-
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-
-    }
-
-    componentWillUpdate(nextProps, nextState) {
-
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-
+        window.addEventListener('keydown', this.handleKeyDown);
+        window.addEventListener('keyup', this.handleKeyUp);
+        window.addEventListener('keyright', this.handleKeyRight);
+        window.addEventListener('keyleft', this.handleKeyLeft);
     }
 
     componentWillUnmount() {
+        window.removeEventListener('keydown', this.handleKeyDown);
+        window.removeEventListener('keyup', this.handleKeyUp);
+        window.removeEventListener('keyright', this.handleKeyRight);
+        window.removeEventListener('keyleft', this.handleKeyLeft);
+    }
 
+    rotate = (forward) => {
+        if (forward) {
+            this.setState({index: this.state.index + 1});
+            box.style.transform = `rotateX(${this.state.index * 60}deg)`;
+        } else {
+            this.setState({index: this.state.index - 1});
+            box.style.transform = `rotateX(${this.state.index * 60}deg)`;
+        }
     }
 
     render() {
         return (
-            <div>
-
+            <div className='wrapper'>
+                <div className="container">
+                    <div className="box">
+                        <div className="card" id="face1">X</div>
+                        <div className="card" id="face2">Y</div>
+                        <div className="card" id="face3">Z</div>
+                        <div className="card" id="face4">A</div>
+                        <div className="card" id="face5">E</div>
+                        <div className="card" id="face6">C</div>
+                    </div>
+                </div>
+                <button id="nextButton" onClick={this.rotate(1)}>Next</button>
+                <button id="prevButton" onClick={this.rotate(-1)}>Previous</button>
             </div>
         );
     }
 }
-
-LetterCube.propTypes = {};
 
 export default LetterCube;
