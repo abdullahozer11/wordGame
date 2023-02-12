4ca2ad786a7ec1fc651c2fb95f5f62b9abe1e550
Abdullah OZER <abdullahozer11@hotmail.com>
2023-02-12 22:42:24 +0100

    certain changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Board from "./components/Board/Board";
-// import Welcome from "./components/Welcome/Welcome";
+// import Board from "./components/Board/Board";
+import Welcome from "./components/Welcome/Welcome";
 
 function App() {
   return (
     <div className="App">
-      {/*<Welcome/>*/}
-      <Board/>
+      <Welcome/>
+      {/*<Board/>*/}
     </div>
   );
 }
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -29,139 +29,118 @@ class Welcome extends Component {
     play() {
         console.log('playing');
     }
-
+    
     render() {
         return (
             <div className={'welcome-div'}>
                 <div className={'tiles-row'}>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-11'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-12'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-13'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-14'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-15'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-16'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-17'}>X</div>
                         </div>
                     </div>
                 </div>
                 <div className={'tiles-row'}>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-21'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-22'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-23'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-24'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-25'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-26'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-26'}>X</div>
                         </div>
                     </div>
                 </div>
                 <div className={'tiles-row'}>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-31'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-32'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-33'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-34'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-35'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-36'}>X</div>
                         </div>
                     </div>
                     <div className="tile-container">
                         <div className={'tile-box'}>
-                            <div className={'tile-front'}>X</div>
-                            <div className={'tile-back'}>Y</div>
+                            <div className={'tile'} id={'tile-37'}>X</div>
                         </div>
                     </div>
                 </div>
