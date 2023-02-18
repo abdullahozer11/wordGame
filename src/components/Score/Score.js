657f62d3415bfd97255cca957932206ee0c16997
Abdullah OZER <abdullahozer11@hotmail.com>
2023-02-18 16:23:24 +0100

    checkpoint

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -8,6 +8,11 @@ class Score extends Component {
             <div className={'score-wrapper'}>
                 <p>SCORE:</p>
                 <p>{this.props.score}</p>
+                <div className="wordListContainer">
+                    <ul className="playerWordList">
+                        {this.props.playedWords.map(word => <li key={word}>{word}</li>)}
+                    </ul>
+                </div>
             </div>
         );
     }
